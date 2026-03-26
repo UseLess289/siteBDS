11f2506c74469056afc89faddfd2cb773d6400e5
Gabriel Derozier-Priotto <gabriel.derozier--priotto@bordeaux-inp.fr>
2026-03-26 23:51:50 +0100

    allo

--- commande.js
+++ commande.js
@@ -28,8 +28,10 @@ const MENUS = {
         { nom: 'Garro', prix: 5.50 },
         { nom: 'Le J, le S', prix: 1.50 },
         { nom: 'Boisson du bar eheheheh', prix: 3.00 },
-        //{ nom: 'Menage/Vaisselle', prix: 0.00 },
-        //{ nom: 'Chorée', prix: 0.00 },
+        { nom: 'Menage/Vaisselle', prix: 0.00 },
+        { nom: 'Chorée', prix: 0.00 },
+        { nom: 'Taxi VROUUUM', prix: 0.00 },
+        { nom: 'Tiramisu café', prix: 3.00 },
     ],
     pessac: [
         { nom: 'Crêpe sucre', prix: 0.10 },
